7c2c8f735c83432361631befb3a6b8eb041ec370
MatheusSFDev <mateussousa909091@gmail.com>
2025-10-03 16:10:31 +0200

    Creación Base de Datos

---
+++ DB.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE bibliotecaOnline;
+USE bibliotecaOnline;
+
+CREATE TABLE usuarios (
+    email varchar(255) primary key,
+    nombre varchar(100) not null,
+    passwd varchar(255) not null
+);
+
+CREATE TABLE juegos (
+    id int primary key auto_increment,
+    titulo varchar(200) not null,
+    descripcion text,
+    autor varchar(100),
+    caratula varchar(255),
+    categoria varchar(50),
+    url varchar(500),
+    año smallint,
+    propietario varchar(255),
+
+    constraint jue_pro_fk foreign key (propietario) references usuarios(email)
+);
