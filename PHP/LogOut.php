0ddc7b3c9b8841e73faddc45d1df96f7a74f5a8d
MatheusSFDev <mateussousa909091@gmail.com>
2025-10-20 16:40:12 +0100

    Borrado de Cookie al Log-Out

--- PHP/LogOut.php
+++ PHP/LogOut.php
@@ -2,5 +2,20 @@
 session_start();
 session_destroy();
 
+require 'Conexion_DB.php';
+if (isset($_COOKIE["cookieSesion"])) {
+    try {
+        $sentencia = $conn->prepare("DELETE FROM cookies WHERE cookie LIKE(:cookie)");
+        $sentencia->bindParam(":cookie", $_COOKIE["cookieSesion"]);
+        $sentencia->execute();
+
+    } catch (PDOException $ex) {
+        // ERROR
+    }
+
+    setcookie("cookieSesion", "", time() - (60 * 60), "/");
+}
+
 header("Location: Login/Login.php");
+exit;
 ?>
